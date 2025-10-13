88fb5b0705469c397b015416af99e7d2e3ada085
MINTS189 <joshuavladimhieryutuc@gmail.com>
2025-10-13 22:16:14 +0800

    task

---
+++ App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddTaskView from "./AddTaskView";
+import TaskListView from "./TaskListView";
+
+function App() {
+  return (
+    <Router>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+        <div className="container">
+          <Link className="navbar-brand" to="/">Task Manager</Link>
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item"><Link className="nav-link" to="/">Task List</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/add">Add Task</Link></li>
+          </ul>
+        </div>
+      </nav>
+
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<TaskListView />} />
+          <Route path="/add" element={<AddTaskView />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
