1a4f6f168e3545ddb4f2c5c613d67debb0d8f807
MINTS189 <joshuavladimhieryutuc@gmail.com>
2025-10-13 22:14:10 +0800

    task

---
+++ TaskListView.js
@@ -0,0 +1,10 @@
+function TaskListView() {
+  return (
+    <div>
+      <h2>Task List View</h2>
+      <p>Task list will be displayed here later.</p>
+    </div>
+  );
+}
+
+export default TaskListView;
